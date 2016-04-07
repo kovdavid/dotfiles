81994d2811567d893d1f856c146f737ebcfeb80c
Davs <kovdavid@gmail.com>
2016-04-07 10:28:57 +0200

    i3_custom_status: replace tabs with spaces

--- bin/i3_custom_status.sh
+++ bin/i3_custom_status.sh
@@ -5,9 +5,9 @@ do
     I3_ORIENTATION=$(cat /tmp/.i3_split_orientation);
     I3_ORIENTATION_STATUS=""
     if [ "$I3_ORIENTATION" == "horizontal" ] ; then
-    	I3_ORIENTATION_STATUS="H"
-	else
-    	I3_ORIENTATION_STATUS="_"
-	fi
-	echo "${line/[/[{\"full_text\":\"I3Split: $I3_ORIENTATION_STATUS\"\},}" || exit 1
+        I3_ORIENTATION_STATUS="H"
+    else
+        I3_ORIENTATION_STATUS="_"
+    fi
+    echo "${line/[/[{\"full_text\":\"I3Split: $I3_ORIENTATION_STATUS\"\},}" || exit 1
 done
