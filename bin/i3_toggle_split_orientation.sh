f6f644367e0b96e7f8840bd41b14ec00da7164f5
Davs <kovdavid@gmail.com>
2016-08-08 18:01:45 +0200

    i3_toggle_split_orientation: refactoring

--- bin/i3_toggle_split_orientation.sh
+++ bin/i3_toggle_split_orientation.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ ! -f /tmp/.i3_split_orientation ] ; then
-    echo -n "horizontal" > /tmp/.i3_split_orientation
+FILE="/tmp/.i3_split_orientation"
+
+if [ ! -f $FILE ] ; then
+    echo -n "horizontal" > $FILE
     i3-msg -tcommand split horizontal
 else
-    ORIENTATION=$(cat /tmp/.i3_split_orientation)
+    ORIENTATION=$(cat $FILE)
     if [ "$ORIENTATION" == "horizontal" ] ; then
-        echo -n "vertical" > /tmp/.i3_split_orientation
+        echo -n "vertical" > $FILE
         i3-msg -tcommand split vertical
     elif [ "$ORIENTATION" == "vertical" ] ; then
-        echo -n "horizontal" > /tmp/.i3_split_orientation
+        echo -n "horizontal" > $FILE
         i3-msg -tcommand split horizontal
     else
         echo "Wrong orientation $ORIENTATION"
