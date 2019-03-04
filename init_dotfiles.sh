eed0333cb59299d487157ca4a9a82f0afe7ad49b
Dávid Kovács <kovdavid@gmail.com>
2019-03-04 08:24:59 +0100

    2019-03-04 08:24:59

--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -91,8 +91,6 @@ ln -s ~/.vim ~/.nvim
 rm -rf ~/.config/redshift.conf
 ln -s ~/dotfiles/redshift.conf ~/.config/redshift.conf
 
-ln -s ~/dotfiles/compton.conf ~/.config/compton.conf
-
 mkdir -p ${XDG_CONFIG_HOME:=~/.config}
 rm -rf $XDG_CONFIG_HOME/nvim
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
