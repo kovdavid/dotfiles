62dd9af4e008d9571a092eacd63b8ca4ebedfb82
Dávid Kovács <david.kovacs@nike.sk>
2021-01-11 15:53:00 +0100

    2021-01-11 15:53:00

--- bin/perl_find_module.pl
+++ bin/perl_find_module.pl
@@ -11,6 +11,8 @@ use File::Basename;
 
 my ($input_file, $line_number, $column, $type) = @ARGV;
 
+my $DEBUG = 0;
+
 # $input_file = "/usr/nikesoft/trading-platform/lib/Prematch/UFO/Fixtures.pm";
 # $line_number = 54;
 # $column = 30;
@@ -18,74 +20,164 @@ my ($input_file, $line_number, $column, $type) = @ARGV;
 
 exit unless $input_file && $line_number;
 
-open my $log_fh, ">", "/tmp/perl_find_module.log" or die "open log: $!";
-say $log_fh join(" ", @ARGV);
-close $log_fh;
+my_log(join(" ", "REQUEST", @ARGV));
+my $response = get_response();
+my_log(join(" ", "RESPONSE", $response));
 
-if (($type // -1) == 5) {
-	my $dir = $input_file;
-	while ($dir ne '/') {
-		$dir = dirname($dir);
-		if ( -f "$dir.pm" ) {
-			say "$dir.pm\t0";
-		}
-	}
-	exit;
+if ($response) {
+	say $response;
 }
 
-open my $fh, "<", $input_file or die "open $input_file: $!";
-my @lines = <$fh>;
-close $fh;
-
-my $line = $lines[$line_number-1];
-
-chomp $line;
-
-exit unless $line;
-
-if ($line =~ m#((?:(?:[A-Z][A-Za-z]*::)+)\w+)\(#) {
-	# Napr Prematch::Request::ManualManageKS::DeleteBet::collect_changes(
-
-	my @tokens = split("::", $1);
-
-	my $function = pop @tokens;
-
-	my $file = join("/", @tokens).".pm";
-
-	my $cmd = "fd -c never -p '$file' $ENV{NIKE_HOME}/ | xargs grep -n -m 1 -H '^sub $function'";
-	my @res = `$cmd`;
-
-	if (scalar(@res)) {
-		my ($file, $line, undef) = split(":", $res[0]);
-		say "$file\t$line";
+sub get_response {
+	if (($type // -1) == 5) {
+		my $dir = $input_file;
+		while ($dir ne '/') {
+			$dir = dirname($dir);
+			if ( -f "$dir.pm" ) {
+				return "$dir.pm\t0";
+			}
+		}
+		return;
 	}
-} elsif ($line =~ m#((?:(?:[A-Z][A-Za-z_]*(?:::)?)+))#) {
-	# Napr Prematch::Request::ManualManageKS::DeleteBet
 
-	my @tokens = split("::", $1);
+	open my $fh, "<", $input_file or die "open $input_file: $!";
+	my @lines = <$fh>;
+	close $fh;
 
-	# Prematch::Request::ManualManageKS::DeleteBet
+	chomp $_ for @lines;
 
-	my $file = "lib/".join("/", @tokens).".pm";
-	my $cmd = "fd -c never -p '$file' $ENV{NIKE_HOME}/";
-	my @res = `$cmd`;
+	my $aliases = get_file_aliases(\@lines);
 
-	if (scalar(@res)) {
-		my $file = $res[0];
-		chomp $file;
-		say "$file\t0";
-	} else {
-		# Prematch::Request::ManualManageKS::DeleteBet - DeleteBet je asi nejaka konstanta
+	my $line = $lines[$line_number-1];
 
-		my $const = pop @tokens;
+	return unless $line;
 
-		my $file = "lib/".join("/", @tokens).".pm";
-		my $cmd = "fd -c never -p '$file' $ENV{NIKE_HOME}/ | xargs grep -n -m 1 -H '$const'";
+	my_log("line $line");
+
+	return
+		undef
+		// check_type1($line)
+		// check_type2($line)
+		// check_type3($line, $aliases)
+		// check_type4($line, $aliases)
+		// "";
+}
+
+# Prematch::Request::ManualManageKS::DeleteBet::collect_changes(
+sub check_type1 {
+	my ($line) = @_;
+
+	while ($line =~ m#((?:(?:[A-Z][A-Za-z]*::)+)\w+)\(#g) {
+		my @tokens = split("::", $1);
+
+		my $function = pop @tokens;
+
+		my $file = join("/", @tokens).".pm";
+
+		my $cmd = "fd -c never -p '$file' $ENV{NIKE_HOME}/ | xargs grep -n -m 1 -H '^sub $function'";
 		my @res = `$cmd`;
 
 		if (scalar(@res)) {
 			my ($file, $line, undef) = split(":", $res[0]);
-			say "$file\t$line";
+			return "$file\t$line";
 		}
 	}
+
+	return;
+}
+
+# Prematch::Request::ManualManageKS::DeleteBet
+sub check_type2 {
+	my ($line) = @_;
+
+	while ($line =~ m#((?:(?:[A-Z][A-Za-z0-9_]*(?:::)?)+))#g) {
+		my @tokens = split("::", $1);
+
+		# Prematch::Request::ManualManageKS::DeleteBet
+
+		my $file = "lib/".join("/", @tokens).".pm";
+		my $cmd = "fd -c never -p '$file' $ENV{NIKE_HOME}/";
+		my @res = `$cmd`;
+
+		if (scalar(@res)) {
+			my $file = $res[0];
+			chomp $file;
+			return "$file\t0";
+		}
+	}
+
+	return;
+}
+
+# TCH_Kurzlist::ipars_to_bet_params(
+sub check_type3 {
+	my ($line, $aliases) = @_;
+
+	while ($line =~ m#((?:[A-Z][A-Za-z]*::)\w+)\(#g) {
+		my @tokens = split("::", $1);
+		next unless scalar(@tokens) == 2;
+
+		my ($alias, $function) = @tokens;
+
+		my $module = $aliases->{$alias};
+		next unless $module;
+
+		my $file = "lib/".join("/", split("::", $module)).".pm";
+		my $cmd = "fd -c never -p '$file' $ENV{NIKE_HOME}/ | xargs grep -n -m 1 -H '^sub $function'";
+		my @res = `$cmd`;
+
+		if (scalar(@res)) {
+			my ($file, $line, undef) = split(":", $res[0]);
+			return "$file\t$line";
+		}
+	}
+
+	return;
+}
+
+# TCH_Kurzlist::STAV
+sub check_type4 {
+	my ($line, $aliases) = @_;
+
+	#TODO
+
+	return;
+}
+
+sub get_file_aliases {
+	my ($lines) = @_;
+
+	my $result = {};
+	my $found_tpd_alias = 0;
+
+	for my $line (@{ $lines }) {
+		if (not $found_tpd_alias and $line =~ /^use TPDAlias/) {
+			$found_tpd_alias = 1;
+		}
+
+		if ($found_tpd_alias and $line =~ /^\s*$/) {
+			return $result;
+		}
+
+		$line =~ s/^use TPDAlias//;
+
+		if ($found_tpd_alias) {
+			if ($line =~ m#^\s*(\S*)\s*=>\s*('|")([^'"]+)('|")#) {
+				my $alias = $1;
+				my $module = $3;
+				$result->{$alias} = $module;
+			}
+		}
+	}
+
+	return $result;
+}
+
+sub my_log {
+	my ($msg) = @_;
+
+	open my $log_fh, ">>", "/tmp/perl_find_module.log" or die "open log: $!";
+	say $log_fh $msg;
+	# say $msg;
+	close $log_fh;
 }
