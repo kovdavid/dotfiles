abdb3640f51785f02ec6327495833b880175a97a
Dávid Kovács <david.kovacs@vacuumlabs.com>
2023-03-07 07:44:58 +0100

    2023-03-07 07:44:58

--- init/01-init-root.sh
+++ init/01-init-root.sh
@@ -176,8 +176,8 @@ ensure_polkit_config
 mkdir -p /opt/cache
 chown -R davs:davs /opt/cache
 
-sudo mkdir -p /clean_daily
-sudo chown davs:davs /clean_daily
+sudo mkdir -p /clean_daily /clean_monthly
+sudo chown davs:davs /clean_daily /clean_monthly
 
 echo "Enabling fstrim.timer"
 systemctl enable fstrim.timer
