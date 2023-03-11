3364390469b9c17e7dddd4b38d1e67281998ad42
Dávid Kovács <david.kovacs@vacuumlabs.com>
2023-03-11 16:38:31 +0100

    2023-03-11 16:38:31

--- dalsik_layout/keymap_v3.cpp
+++ dalsik_layout/keymap_v3.cpp
@@ -1,8 +1,11 @@
 #include "keymap.h"
 #include "combos.h"
 
+#define CAPS_WORD_TIMEOUT 2000
+
 #define CTRL_ESC  D(MOD_LCTRL, KC_ESCAPE)
 #define SHIFT_SPC D(MOD_LSHIFT, KC_SPACE)
+#define L1_SPACE  DL(1, KC_SPACE)
 #define L3_SCOLON DSL(3, KC_SEMICOLON)
 #define L4_SLASH  DSL(4, KC_SLASH)
 #define L2_BSPC   DSL(2, KC_BACKSPACE)
@@ -40,6 +43,8 @@ const KeyCoords combo15[] PROGMEM = { { 2, 6 }, { 2, 7 } }; // -
 const KeyCoords combo16[] PROGMEM = { { 1, 1 }, { 1, 2 } }; // Tab
 const KeyCoords combo17[] PROGMEM = { { 1, 9 }, { 1, 10 } }; // Enter
 
+const KeyCoords combo18[] PROGMEM = { { 3, 5 }, { 3, 6 } }; // CAPS_WORD
+
 ComboState combos[] = COMBOS({
     COMBO(combo1, OSM(MOD_LSHIFT)),
     COMBO(combo2, OSM(MOD_LGUI)),
@@ -62,6 +67,8 @@ ComboState combos[] = COMBOS({
     COMBO(combo15, KC_MINS), // -
     COMBO(combo16, KC_TAB), // Tab
     COMBO(combo17, KC_ENTER), // Enter
+
+    COMBO(combo18, CAPS_WORD),
 });
 
 const uint32_t keymap[][KEYBOARD_ROWS][KEYBOARD_COLS] PROGMEM = KEYMAP({
@@ -89,10 +96,10 @@ const uint32_t keymap[][KEYBOARD_ROWS][KEYBOARD_COLS] PROGMEM = KEYMAP({
 
     /* Layer 1
      * ┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     * │ Tab    │   \    │        │        │        │        │ │        │   {    │   }    │   [    │   ]    │   `    │
+     * │ Tab    │   \    │        │        │        │        │ │        │   (    │   )    │   [    │   ]    │   `    │
      * │        │        │        │        │        │        │ │        │        │        │        │        │        │
      * ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-     * │ Ctrl   │   !    │   @    │   #    │   $    │   %    │ │   ^    │   &    │   *    │   (    │   )    │ Enter  │
+     * │ Ctrl   │   !    │   @    │   #    │   $    │   %    │ │   ^    │   &    │   *    │   {    │   }    │ Enter  │
      * │ ESC    │        │        │        │        │        │ │        │        │        │        │        │        │
      * ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
      * │ OneShot│   |    │        │        │   =    │   +    │ │   -    │   _    │   <    │   >    │   ?    │ Delete │
@@ -103,8 +110,8 @@ const uint32_t keymap[][KEYBOARD_ROWS][KEYBOARD_COLS] PROGMEM = KEYMAP({
      * └────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
      */
     LAYOUT_4x12(
-        _______,  KC_BSLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_GRV,
-        _______,  KC_EXLM,   KC_AT ,  KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  XXXXXXX,  XXXXXXX,  _______,
+        _______,  KC_BSLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LPRN,  KC_RPRN,  KC_LBRC,  KC_RBRC,  KC_GRV,
+        _______,  KC_EXLM,   KC_AT ,  KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LCBR,  KC_RCBR,  _______,
         _______,  KC_PIPE,  XXXXXXX,  XXXXXXX,  KC_EQL,   KC_PLUS,  KC_MINS,  KC_UNDS,  KC_LABK,  KC_RABK,  KC_QUES,  _______,
         _______,  _______,  _______,  _______,  _______,  KC_SPC,   KC_BSPC,  _______,  _______,  LHT(1),   KC_UP,    KC_DOWN
     ),
