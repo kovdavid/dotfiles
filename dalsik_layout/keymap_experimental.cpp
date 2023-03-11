f140b1c68ef7b57e4e3668c63b7122f7dea20280
Dávid Kovács <david.kovacs@vacuumlabs.com>
2023-03-11 16:42:13 +0100

    2023-03-11 16:42:13

--- dalsik_layout/keymap_experimental.cpp
+++ dalsik_layout/keymap_experimental.cpp
@@ -5,7 +5,7 @@
 
 #define LDEFAULT 0
 #define LNUM 1
-#define LNAV 2
+#define LNAV 3
 #define LGUI 4
 
 #define OSM_SHFT  OSM(MOD_LSHIFT)
@@ -90,8 +90,6 @@ const uint32_t keymap[][KEYBOARD_ROWS][KEYBOARD_COLS] PROGMEM = KEYMAP({
         OSM_CTRL,  OSM_SHFT,  XXXXXXX,  CTRL_ESC,  SHIFT_SPC,  LNUM_TAB,    /**/  KC_BSPC,     LNAV_ENT,  KC_DELETE,  XXXXXXX,  XXXXXXX,  LHT(LNAV),
     ),
 
-
-
     /* Layer NUM
        ┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
        │        │        │        │        │        │        │ │        │   [    │   7    │   8    │   9    │   ]    │
@@ -112,29 +110,6 @@ const uint32_t keymap[][KEYBOARD_ROWS][KEYBOARD_COLS] PROGMEM = KEYMAP({
         KC_LGUI,   KC_LSFT,   KC_LCTL,  KC_LALT,  XXXXXXX,  XXXXXXX,   /**/  XXXXXXX,  KC_EQL,   KC_4,       KC_5,     KC_6,     KC_QUOT,
         XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   /**/  XXXXXXX,  KC_BSLS,  KC_1,       KC_2,     KC_3,     KC_GRV,
         OSM_CTRL,  OSM_SHFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  LNUM_TAB,  /**/  KC_BSPC,  KC_0,     KC_DELETE,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    ),
-
-
-    /* Layer NAV
-       ┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
-       │  F1    │  F2    │  F3    │  F4    │        │ VolUp  │ │ Screen │        │  PgUp  │   Up   │  PgDn  │        │
-       │        │        │        │        │        │        │ │ Saver  │        │        │        │        │        │
-       ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-       │  F5    │  F6    │  F7    │  F8    │        │ VolDn  │ │        │ Shift+ │  Left  │  Down  │  Right │        │
-       │        │        │        │        │        │        │ │        │ Insert │        │        │        │        │
-       ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-       │  F9    │  F10   │  F11   │  F12   │        │ VolMut │ │        │        │  Home  │ Insert │  End   │        │
-       │        │        │        │        │        │        │ │        │        │        │        │        │        │
-       ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-       │  OSM   │  OSM   │        │  CTRL  │ SHIFT  │ L NUM  │ │ BSPACE │ L NAV  │ DELETE │        │        │        │
-       │  CTRL  │  SHIFT │        │ ESCAPE │ SPACE  │   TAB  │ │        │ RETURN │        │        │        │        │
-       └────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
-     */
-    LAYOUT_4x12(
-        KC_F1,     KC_F2,     KC_F3,    KC_F4,     XXXXXXX,    KC_VOLUP,  /**/  KC_SCR_SVR,  XXXXXXX,   KC_PGUP,    KC_UP,    KC_PGDN,   XXXXXXX,
-        KC_F5,     KC_F6,     KC_F7,    KC_F8,     XXXXXXX,    KC_VOLDN,  /**/  XXXXXXX,     SHFT_INS,  KC_LEFT,    KC_DOWN,  KC_RIGHT,  XXXXXXX,
-        KC_F9,     KC_F10,    KC_F11,   KC_F12,    XXXXXXX,    KC_MUTE,   /**/  XXXXXXX,     XXXXXXX,   KC_HOME,    KC_INS,   KC_END,    XXXXXXX,
-        OSM_CTRL,  OSM_SHFT,  XXXXXXX,  CTRL_ESC,  SHIFT_SPC,  LNUM_TAB,  /**/  KC_BSPC,     LNAV_ENT,  KC_DELETE,  XXXXXXX,  XXXXXXX,   XXXXXXX,
     ),
 
     /* Layer 3
@@ -159,6 +134,27 @@ const uint32_t keymap[][KEYBOARD_ROWS][KEYBOARD_COLS] PROGMEM = KEYMAP({
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
     ),
 
+    /* Layer NAV
+       ┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
+       │  F1    │  F2    │  F3    │  F4    │        │ VolUp  │ │ Screen │        │  PgUp  │   Up   │  PgDn  │        │
+       │        │        │        │        │        │        │ │ Saver  │        │        │        │        │        │
+       ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
+       │  F5    │  F6    │  F7    │  F8    │        │ VolDn  │ │        │ Shift+ │  Left  │  Down  │  Right │        │
+       │        │        │        │        │        │        │ │        │ Insert │        │        │        │        │
+       ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
+       │  F9    │  F10   │  F11   │  F12   │        │ VolMut │ │        │        │  Home  │ Insert │  End   │        │
+       │        │        │        │        │        │        │ │        │        │        │        │        │        │
+       ├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
+       │  OSM   │  OSM   │        │  CTRL  │ SHIFT  │ L NUM  │ │ BSPACE │ L NAV  │ DELETE │        │        │        │
+       │  CTRL  │  SHIFT │        │ ESCAPE │ SPACE  │   TAB  │ │        │ RETURN │        │        │        │        │
+       └────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
+     */
+    LAYOUT_4x12(
+        KC_F1,     KC_F2,     KC_F3,    KC_F4,     XXXXXXX,    KC_VOLUP,  /**/  KC_SCR_SVR,  XXXXXXX,   KC_PGUP,    KC_UP,    KC_PGDN,   XXXXXXX,
+        KC_F5,     KC_F6,     KC_F7,    KC_F8,     XXXXXXX,    KC_VOLDN,  /**/  XXXXXXX,     SHFT_INS,  KC_LEFT,    KC_DOWN,  KC_RIGHT,  XXXXXXX,
+        KC_F9,     KC_F10,    KC_F11,   KC_F12,    XXXXXXX,    KC_MUTE,   /**/  XXXXXXX,     XXXXXXX,   KC_HOME,    KC_INS,   KC_END,    XXXXXXX,
+        OSM_CTRL,  OSM_SHFT,  XXXXXXX,  CTRL_ESC,  SHIFT_SPC,  LNUM_TAB,  /**/  KC_BSPC,     LNAV_ENT,  KC_DELETE,  XXXXXXX,  XXXXXXX,   XXXXXXX,
+    ),
 
     /* Layer GUI
        ┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
