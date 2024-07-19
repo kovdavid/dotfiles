34978044fe1edae32dbcdcc6c1a51b76880d5fe1
Dávid Kovács <kovdavid@gmail.com>
2024-07-19 07:45:50 +0200

    2024-07-19 07:45:50

--- nvim/lua/user/plugins/typescript-tools.lua
+++ nvim/lua/user/plugins/typescript-tools.lua
@@ -5,6 +5,17 @@ return {
         settings = {
             separate_diagnostic_server = false,
             tsserver_max_memory = 1024,
+        },
+        on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+        jsx_close_tag = {
+            enable = true,
+            filetypes = {
+                "javascriptreact",
+                "typescriptreact",
+            }
         }
     },
 }
