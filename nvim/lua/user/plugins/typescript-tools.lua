82956cb59790b9e0191794d59f2aeb941ffd431e
Dávid Kovács <kovdavid@gmail.com>
2024-07-26 06:58:53 +0200

    2024-07-26 06:58:53

--- nvim/lua/user/plugins/typescript-tools.lua
+++ nvim/lua/user/plugins/typescript-tools.lua
@@ -3,8 +3,8 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
         settings = {
-            separate_diagnostic_server = false,
-            tsserver_max_memory = 1024,
+            -- separate_diagnostic_server = false,
+            -- tsserver_max_memory = 1536,
         },
         on_attach = function(client, bufnr)
             client.server_capabilities.documentFormattingProvider = false
