ed18d39a4fc98c813437af476530f449ea9068a9
Dávid Kovács <kovdavid@gmail.com>
2026-05-01 10:29:22 +0200

    2026-05-01 10:29:22

--- nvim/lua/user/plugins/diffview.lua
+++ nvim/lua/user/plugins/diffview.lua
@@ -7,6 +7,19 @@ return {
         disable_diagnostics = true,
       },
     },
+    file_panel = {
+      -- listing_style = function()
+      --   local listing_style = vim.api.nvim_win_get_width(0) < 150 and "list" or "tree"
+      --   return { listing_style = listing_style }
+      -- end,
+      listing_style = "tree",
+      win_config = function()
+        local width = vim.api.nvim_win_get_width(0) < 150 and 25 or 35
+        return {
+          width = width,
+        }
+      end,
+    },
   },
   keys = {
     { "<leader>do", ":DiffviewOpen", desc = "DiffviewOpen", silent = false },
