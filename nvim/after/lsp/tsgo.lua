8577ca2a99a0141063fc9850b6b4985d2318a319
Dávid Kovács <kovdavid@gmail.com>
2026-07-17 15:01:04 +0200

    2026-07-17 15:01:04

--- nvim/after/lsp/tsgo.lua
+++ nvim/after/lsp/tsgo.lua
@@ -1,4 +1,5 @@
 return {
+    cmd = { "tsc", "--lsp", "--stdio" },
     root_dir = function(bufnr, on_dir)
         local project_root = vim.fs.root(bufnr, { ".vimproject", ".git" }) or vim.fn.getcwd()
         on_dir(project_root)
