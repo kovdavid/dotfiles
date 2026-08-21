0b17e8a19512faa1df4afe66a973149bfe7b2fa8
Dávid Kovács <kovdavid@gmail.com>
2026-08-21 17:43:46 +0200

    2026-08-21 17:43:46

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -4,8 +4,8 @@ return {
     vim.lsp.config("*", { capabilities = require("mini.completion").get_lsp_capabilities() })
 
     -- vim.lsp.enable("eslint")
-    -- vim.lsp.enable("vtsls")
-    vim.lsp.enable("tsc")
+    vim.lsp.enable("vtsls")
+    -- vim.lsp.enable("tsc")
     vim.lsp.enable("clangd")
 
     for _, lhs in ipairs({ "grn", "gra", "grr", "gri", "grt", "grx" }) do
