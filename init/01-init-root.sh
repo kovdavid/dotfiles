e6947009402e22d73a9fe32b4718128f8dec8508
Dávid Kovács <kovdavid@gmail.com>
2026-08-30 21:34:31 +0200

    2026-08-30 21:34:31

--- init/01-init-root.sh
+++ init/01-init-root.sh
@@ -219,7 +219,7 @@ ensure_polkit_config
 ensure_nginx_pacman_hook
 ensure_systemd_sleep_entry
 
-for dir in "/opt/cache" "/opt/javascript" "/opt/clean_daily" "/opt/clean_manually"; do
+for dir in "/opt/AndroidSDK/home-folder" "/opt/claude" "/opt/cache" "/opt/javascript/yarn" "/opt/javascript/npm" "/opt/pip" "/opt/flatpak/var" "/opt/gradle" "/opt/clean_daily" "/opt/clean_manually"; do
     mkdir -p "$dir"
     chown davs:davs "$dir"
 done
